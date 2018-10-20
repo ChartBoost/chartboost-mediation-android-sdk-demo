8a2c587f409fd603781b713672f6dd9c9c418391
Virendra Shakya <drvirens@gmail.com>
2018-10-20 01:22:21 -0700

    upgrade tools

--- android.helium.sdk/helium/HeliumTestApp/helium-infrastructure/src/test/java/com/chartboost/helium/helium_infrastructure/HeliumInterstitialAd_UnitTest.java
+++ android.helium.sdk/helium/HeliumTestApp/helium-infrastructure/src/test/java/com/chartboost/helium/helium_infrastructure/HeliumInterstitialAd_UnitTest.java
@@ -2,8 +2,11 @@ package com.chartboost.helium.helium_infrastructure;
 import static org.mockito.Mockito.*;
 import com.chartboost.helium.helium_common.event.EventBus;
 import com.chartboost.helium.helium_infrastructure.internal.HeliumInterstitialAdImpl;
+import com.chartboost.helium.helium_interactors.RepoFactory;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
 import static org.junit.Assert.*;
 
 public class HeliumInterstitialAd_UnitTest {
@@ -19,11 +22,13 @@ public class HeliumInterstitialAd_UnitTest {
 
     @Test
     public void interstitialAd_GetProper() {
-        EventBus eventBus = null;
+        EventBus eventBus = Mockito.mock(EventBus.class);
+        String p = "placementid";
+        RepoFactory repoFactory = Mockito.mock(RepoFactory.class);
         HeliumInterstitialAd interstitialAd = new HeliumInterstitialAdImpl.Builder()
-            .setEventBus(null)
-            .setPlacementId(null)
-            .setRepoFactory(null)
+            .setEventBus(eventBus)
+            .setPlacementId(p)
+            .setRepoFactory(repoFactory)
             .build();
         assertTrue(interstitialAd != null);
     }
