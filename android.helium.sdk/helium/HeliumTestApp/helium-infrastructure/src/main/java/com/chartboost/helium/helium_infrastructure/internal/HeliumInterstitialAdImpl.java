d30804c4bcaebe096c219123692c9e89b60412d1
Virendra Shakya <drvirens@gmail.com>
2018-10-19 19:19:00 -0700

    simple helium ad builder

--- android.helium.sdk/helium/HeliumTestApp/helium-infrastructure/src/main/java/com/chartboost/helium/helium_infrastructure/internal/HeliumInterstitialAdImpl.java
+++ android.helium.sdk/helium/HeliumTestApp/helium-infrastructure/src/main/java/com/chartboost/helium/helium_infrastructure/internal/HeliumInterstitialAdImpl.java
@@ -1,9 +1,11 @@
 package com.chartboost.helium.helium_infrastructure.internal;
 
+import com.chartboost.helium.helium_common.event.EventBus;
 import com.chartboost.helium.helium_infrastructure.HeliumInterstitialAd;
 import com.chartboost.helium.helium_infrastructure.HeliumInterstitialAdDelegate;
 import com.chartboost.helium.helium_infrastructure.NoAdFoundxception;
 import com.chartboost.helium.helium_infrastructure.UIViewController;
+import com.chartboost.helium.helium_interactors.RepoFactory;
 
 public class HeliumInterstitialAdImpl implements HeliumInterstitialAd {
     @Override
@@ -18,6 +20,26 @@ public class HeliumInterstitialAdImpl implements HeliumInterstitialAd {
 
     @Override
     public void showAdWithViewController(UIViewController vc, HeliumInterstitialAdDelegate delegate) throws NoAdFoundxception {
+
+    }
+
+    public static class Builder {
+        public Builder setPlacementId(String placementId) {
+            return this;
+        }
+
+        public Builder setEventBus(EventBus eventBus) {
+            return this;
+        }
+
+        public Builder setRepoFactory(RepoFactory repoFactory) {
+            return this;
+        }
+
+        public HeliumInterstitialAd build() {
+            HeliumInterstitialAdImpl interstitialAd = new HeliumInterstitialAdImpl();
+            return interstitialAd;
+        }
 
     }
 }
