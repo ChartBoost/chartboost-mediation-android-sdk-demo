06449473444df40e8157eefbb407c073237b6c20
Virendra Shakya <drvirens@gmail.com>
2018-10-21 16:45:00 -0700

    repo name

--- android.helium.sdk/helium/HeliumTestApp/helium-interactors/src/main/java/com/chartboost/helium/helium_interactors/RepoFactory.java
+++ android.helium.sdk/helium/HeliumTestApp/helium-interactors/src/main/java/com/chartboost/helium/helium_interactors/RepoFactory.java
@@ -1,11 +1,14 @@
 package com.chartboost.helium.helium_interactors;
 
+import android.util.Log;
+
 import com.chartboost.helium.helium_domain.BasicIdentifier;
 import com.chartboost.helium.helium_domain.ad.AdAggregate;
 import com.chartboost.helium.helium_interactors.repos.CompletionCallback;
 import com.chartboost.helium.helium_interactors.repos.HeliumRepo;
 
 public final class RepoFactory {
+    private static String TAG = "RepoFactory";
     private final HeliumRepo heliumRepo_;
     private RepoFactory(final HeliumRepo heliumRepo) {
         heliumRepo_ = heliumRepo;
@@ -19,6 +22,11 @@ public final class RepoFactory {
     }
 
     public void getAd(BasicIdentifier adid, CompletionCallback completion) {
+        if (adid == null) {
+            Log.d(TAG, "adid is null, so this should come from cloud");
+        } else {
+            Log.d(TAG, "adid is not null, so may come from memory or file store");
+        }
         AdAggregate adAggregate = null;
 
         adAggregate = new AdAggregate();
--- android.helium.sdk/helium/HeliumTestApp/helium-interactors/src/main/java/com/chartboost/helium/helium_interactors/repos/HeliumRepo.java
+++ android.helium.sdk/helium/HeliumTestApp/helium-interactors/src/main/java/com/chartboost/helium/helium_interactors/repos/HeliumRepo.java
@@ -1,6 +1,10 @@
 package com.chartboost.helium.helium_interactors.repos;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class HeliumRepo {
+    private ConcurrentHashMap<RepoName, Object> loadedRepos_;
+
     public void autoloadAllRepos() {
 
     }
