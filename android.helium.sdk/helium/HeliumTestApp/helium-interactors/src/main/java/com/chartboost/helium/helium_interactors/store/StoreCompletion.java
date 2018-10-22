6e88d25c7d2cc806256d62665453dba7679b9a74
Virendra Shakya <drvirens@gmail.com>
2018-10-21 21:00:19 -0700

    store completion callback

--- android.helium.sdk/helium/HeliumTestApp/helium-interactors/src/main/java/com/chartboost/helium/helium_interactors/store/StoreCompletion.java
+++ android.helium.sdk/helium/HeliumTestApp/helium-interactors/src/main/java/com/chartboost/helium/helium_interactors/store/StoreCompletion.java
@@ -3,7 +3,16 @@ package com.chartboost.helium.helium_interactors.store;
 import com.chartboost.helium.helium_common.OnError;
 import com.chartboost.helium.helium_common.OnSuccess;
 
-public class StoreCompletion<AggregateT> {
-    private OnSuccess<AggregateT> successCallback_;
-    private OnError errorCallback_;
+public final class StoreCompletion<AggregateT> {
+    private final OnSuccess<AggregateT> successCallback_;
+    private final OnError errorCallback_;
+
+    private StoreCompletion(OnSuccess<AggregateT> successCallback, OnError errorCallback) {
+        successCallback_ = successCallback;
+        errorCallback_ = errorCallback;
+    }
+
+    public static StoreCompletion of(OnSuccess successCallback, OnError errorCallback) {
+        return new StoreCompletion(successCallback, errorCallback);
+    }
 }
